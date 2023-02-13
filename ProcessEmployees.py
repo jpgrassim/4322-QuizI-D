4a4cd0cd0e4e08cfe8f50b872805efa73649e1a1
Juampi <juanpablo_grassimaz1@baylor.edu>
2023-02-13 13:45:06 -0600

    juampi

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -12,23 +12,43 @@ import csv
 
 #open the file
 
-
-
+infile = open('employee_data.csv', 'r', newline = '')
+reader = csv.reader(infile)
+fields = next(reader)
 
 #create an empty dictionary
-
+emp_list = []
+emp_dict = {}
 
 #use a loop to iterate through the csv file
+
+for row in reader:
+     if row[3] == 'Marketing':
+        emp_list.append(row[1])
+        emp_list.append(row[2])
+        emp_list.append(row[5])
+        for emp in emp_list: 
+            full_name = row[1] + row[2]
+            emp_dict[full_name] = row[5]
+
 
 
     #check if the employee fits the search criteria
 
+for row in reader:
+    if row[3] == 'Marketing':
+        print('Manager name: ', row[1], row[2], 'Salary: ', row[5])
 
     
 
 print()
 print('=========================================')
 print()
+
+for emp in emp_dict:
+    emp_dict[emp] = float(emp_dict[emp]) * 1.10
+    for k,j in emp_dict.items():
+        print('Manager Name: ', k, 'Salary: ', j )
 
 #iternate through the dictionary and print out the key and value as per printout
 
